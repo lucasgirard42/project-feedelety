3ba1db4569b8549fc5a3619058243dca8d580d79
Girard lucas <pro.lucas.girard@gmail.com>
2020-10-07 11:04:34 +0200

    fix bug style grey in listItem.js, add native-base and App.js

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { AppLoading } from 'expo';
+import { Container, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import Providers from './navigation';
 
-export default function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render(){
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
   return (
-  <Providers />
+    <Container>
+      <Providers />
+    </Container>
   )
+  }
 }
